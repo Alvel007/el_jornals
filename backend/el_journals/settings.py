c32727eb3d040a22bcebdbd139cd9cd38c5dcca3
Aleksey Kirilishin <alexkak@inbox.ru>
2024-03-13 19:12:36 +0400

    Настройка статики-3

--- backend/el_journals/settings.py
+++ backend/el_journals/settings.py
@@ -104,11 +104,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/backend_static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
-MEDIA_URL = '/backend_media/'
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

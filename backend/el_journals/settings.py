cdde5463c5af516f106a359726d0ed9af751fbe4
Aleksey Kirilishin <alexkak@inbox.ru>
2024-03-13 15:15:07 +0400

    Настройка статики

--- backend/el_journals/settings.py
+++ backend/el_journals/settings.py
@@ -106,6 +106,9 @@ USE_TZ = True
 
 STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
 
 MEDIA_URL = '/backend_media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
